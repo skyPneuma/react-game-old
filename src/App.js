35ec5a5fa57aa1e2447ba11ae9971f89860661c6
sky <sky.soul95@gmail.com>
2021-02-21 17:01:10 +0300

    init: start react-game task

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        test
+        react-game
       </header>
     </div>
-  );
-}
+  )
+};
 
 export default App;
