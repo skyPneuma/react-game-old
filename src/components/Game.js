5eb60d36c011d6c3e9e0c4060929866028991435
sky_pneuma <v.senkecich@intelico.su>
2021-02-22 18:02:18 +0300

    refactor: delete unused components & correct styles

--- src/components/Game.js
+++ src/components/Game.js
@@ -16,13 +16,19 @@ const Game = () => {
 	
 	return <div className="wrapper">
 		<div className="blur">
-			<div>
-				<CountryField country={country}/>
-			</div>
-			<div className="buttons">
+			<div className="buttons buttons_top">
 				<button className="btn__restart">Restart game</button>
 				<button className="btn__hint">Hint</button>
 				<button className="btn__skip" onClick={() => getRandomCountry()}>Skip round</button>
+			</div>
+			<div>
+				<CountryField country={country}/>
+			</div>
+			<div className="buttons buttons_bottom">
+				<button className="btn__veriant">Variant 1</button>
+				<button className="btn__veriant">Variant 2</button>
+				<button className="btn__veriant">Variant 3</button>
+				<button className="btn__veriant">Variant 4</button>
 			</div>
 		</div>
 	</div>;
--- src/components/Square.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './styles.scss';
-
-const Square = ({ onClick, value}) => {
-	return <button onClick={onClick} className="square">{value}</button>;
-};
-
-export default Square;
